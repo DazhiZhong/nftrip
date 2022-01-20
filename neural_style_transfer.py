0d7666f9400451c575cd2b1474595bac1638a10a
Dazhi Zhong <dazhizho@usc.edu>
2022-01-20 07:44:29 +0000

    0

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -26,6 +26,32 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+from huggingface_hub import hf_hub_url, cached_download
+
+MODELS = {
+    'x2': dict(
+        scale=2,
+        repo_id='shonenkov/rudalle-utils',
+        filename='RealESRGAN_x2.pth',
+    ),
+    'x4': dict(
+        scale=4,
+        repo_id='shonenkov/rudalle-utils',
+        filename='RealESRGAN_x4.pth',
+    ),
+    'x8': dict(
+        scale=8,
+        repo_id='shonenkov/rudalle-utils',
+        filename='RealESRGAN_x8.pth',
+    ),
+}
+
+for model in MODELS.keys():
+    cache_dir = 'models'
+    config = MODELS[model]
+    config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
+    cached_download(config_file_url, cache_dir=cache_dir, force_filename=config['filename'])
+
 
 def build_loss(neural_net, optimizing_img, target_representations, content_feature_maps_index, style_feature_maps_indices, config):
     target_content_representation = target_representations[0]
---
+++ runrudale.py
@@ -0,0 +1,35 @@
+#@title rudalle-sr setup
+import os
+import sys
+import torch
+import tempfile
+import numpy as np
+from pathlib import Path
+from PIL import Image
+from rudalle.realesrgan.model import RealESRGAN
+
+
+class ESRGANUpscale():
+    def __init__(self,esrganscale):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print(f"using {device} for esrganupscale")
+        self.model = RealESRGAN(device, esrganscale)
+        self.model.load_weights(f"models/RealESRGAN_x{esrganscale}.pth")
+        print("Model loaded!")
+    def gan_upscale(self,imgpath,outpath,return_image=False):
+        input_image = Image.open(str(imgpath))     
+        input_image = input_image.convert('RGB')
+        with torch.no_grad():
+            sr_image = self.model.predict(np.array(input_image))
+        sr_image.save(outpath)
+        if return_image:
+            return sr_image
+        else:
+            return None
+
+esrgan = ESRGANUpscale(2)
+
+esrgan.gan_upscale("arty3.png","tmp.png")
+
+
+
